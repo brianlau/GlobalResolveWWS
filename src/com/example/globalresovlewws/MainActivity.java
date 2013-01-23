9bbb3cf1b22b7a6417807ceb6f368ad47bbb2dc0
Brian Lau <ir00ber@gmail.com>
2013-01-22 21:28:43 -0700

    Prototype of what mode should do with weather, changes images right now every 5 seconds.  In future update to correspond with weather

--- src/com/example/globalresovlewws/MainActivity.java
+++ src/com/example/globalresovlewws/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
 
@@ -13,7 +15,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		final int []imageArray={R.drawable.sunny_icon,R.drawable.night_rain};
+
+		final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+		final Handler handler = new Handler();
+		         Runnable runnable = new Runnable() {
+		            int i=0;
+		            public void run() {
+		                imageView.setImageResource(imageArray[i]);
+		                i++;
+		                if(i>imageArray.length-1)
+		                {
+		                i=0;    
+		                }
+		                handler.postDelayed(this, 5000);  //for interval...
+		            }
+		        };
+		        handler.postDelayed(runnable, 2000); //for initial delay..
 		Button btnDockedMode = (Button) findViewById(R.id.button1);
 		
 		btnDockedMode.setOnClickListener(new View.OnClickListener() {
