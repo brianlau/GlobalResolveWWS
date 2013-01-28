e3daf03fb66ad4d5468317e4e7bceee021de863a
Brian Lau <ir00ber@gmail.com>
2013-01-27 22:30:15 -0700

    On rain, sounds an alarm, stops when sunny

--- src/com/example/globalresovlewws/SimulateData.java
+++ src/com/example/globalresovlewws/SimulateData.java
@@ -1,13 +1,14 @@
 package com.example.globalresovlewws;
 
+//dummyTemperature Class
 public class SimulateData {
 	private int temp = 0;
 
-	public void TemperatureForecast() {
+	public void TemperatureForecast() {//sets the temp variable
 		temp = (int) (Math.random() * 100);
 	}
 
-	public int CurrentTemperature() {
+	public int CurrentTemperature() { //returns temperature as an int
 		return temp;
 	}
 }
