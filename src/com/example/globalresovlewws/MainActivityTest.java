a27640474b8da9036de34b3a8866bb8cac7c6b60
Brian Lau <ir00ber@gmail.com>
2013-01-29 22:01:38 -0700

    Added Initial test Case, doesn't actually test anything yet, will do in the future

---
+++ src/com/example/globalresovlewws/MainActivityTest.java
@@ -0,0 +1,44 @@
+package com.example.globalresovlewws;
+
+import com.example.globalresovlewws.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	private MainActivity mActivity;
+	private Button myButton;
+	private ImageView mImageView;
+	private TextView mTextView;
+	public static final boolean INITIAL_POSITION = false;
+	public MainActivityTest() {
+		super("com.example.globalresovlewws", MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception{
+		super.setUp();
+		setActivityInitialTouchMode(false);
+		
+		mActivity = getActivity();
+		myButton = (Button) mActivity.findViewById(com.example.globalresovlewws.R.id.button1);
+		mImageView = (ImageView) mActivity.findViewById(com.example.globalresovlewws.R.id.imageView);
+		mTextView = (TextView) mActivity.findViewById(com.example.globalresovlewws.R.id.temp_curr);
+	}
+	
+	public void testMainActivityUI(){
+		mActivity.runOnUiThread(
+				new Runnable(){
+					public void run(){
+						myButton.requestFocus();
+						myButton.setSelected(INITIAL_POSITION);;
+					}
+				}
+				);
+	}
+}
