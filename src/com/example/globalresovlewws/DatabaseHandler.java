e82bdf0f32b99aa46b49f5fb101e6e8de2f60d4d
Brian Lau <ir00ber@gmail.com>
2013-02-04 12:34:25 -0700

    Finished up getWeather, getAllWeather, deleteWeather, and addWeather methods

--- src/com/example/globalresovlewws/DatabaseHandler.java
+++ src/com/example/globalresovlewws/DatabaseHandler.java
@@ -1,9 +1,11 @@
 package com.example.globalresovlewws;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.*;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 
@@ -30,8 +32,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		String CREATE_WEATHER_TABLE = "Create table " + TABLE_WEATHER + "(Lat"
-				+ KEY_LAT + " Long" + KEY_LONG + " Time " + KEY_TIME
+		String CREATE_WEATHER_TABLE = "Create table " + TABLE_WEATHER
+				+ " Time " + KEY_TIME + "Lat" + KEY_LAT + " Long" + KEY_LONG
 				+ "MAX TEMP " + KEY_MAX_TEMP + "MINTEMP" + KEY_MIN_TEMP
 				+ "Precip Chance" + KEY_CHANCE_PREC;
 		db.execSQL(CREATE_WEATHER_TABLE);
@@ -44,9 +46,9 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		onCreate(db);
 	}
 
-	public void addWeather(Weather weather){
+	public void addWeather(Weather weather) {
 		SQLiteDatabase db = this.getWritableDatabase();
-		
+
 		ContentValues values = new ContentValues();
 		values.put(KEY_LAT, weather.getLatitude());
 		values.put(KEY_LONG, weather.getLongitude());
@@ -54,20 +56,56 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		values.put(KEY_CHANCE_PREC, weather.getChanceOfPrecipi());
 		values.put(KEY_MAX_TEMP, weather.getMaxTemp());
 		values.put(KEY_MIN_TEMP, weather.getMinTemp());
-		
+
 		db.insert(TABLE_WEATHER, null, values);
 		db.close();
 	}
-	
-	public Weather getWeather(String time){
-		SQLiteDatabase db = this.getWritableDatabase();
-		
-		return null;
-		
+
+	public Weather getWeather(String time) {
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.query(TABLE_WEATHER,
+				new String[] { KEY_TIME, KEY_LAT, KEY_LONG, KEY_MAX_TEMP,
+						KEY_MIN_TEMP, KEY_CHANCE_PREC }, KEY_TIME + "= ?",
+				new String[] { String.valueOf(time) }, null, null, null, null);
+		if (cursor != null)
+			cursor.moveToFirst();
+
+		Weather weather = new Weather(cursor.getString(0),
+				Double.parseDouble(cursor.getString(1)),
+				Double.parseDouble(cursor.getString(2)),
+				Integer.parseInt(cursor.getString(3)), Integer.parseInt(cursor
+						.getString(4)), Integer.parseInt(cursor.getString(5)));
+		return weather;
+
 	}
-	
-	public List<Weather> getAllWeather(){
-		return null;
-		
+
+	public List<Weather> getAllWeather() {
+		List<Weather> weatherList = new ArrayList<Weather>();
+
+		String selectQuery = "Select from" + TABLE_WEATHER;
+
+		SQLiteDatabase db = this.getWritableDatabase();
+		Cursor cursor = db.rawQuery(selectQuery, null);
+
+		if (cursor.moveToFirst()) {
+			do {
+				Weather weather = new Weather();
+				weather.setTime(cursor.getString(0));
+				weather.setLatitude(Double.parseDouble(cursor.getString(1)));
+				weather.setLongitude(Double.parseDouble(cursor.getString(2)));
+				weather.setMaxTemp(Integer.parseInt(cursor.getString(3)));
+				weather.setMinTemp(Integer.parseInt(cursor.getString(4)));
+				weather.setChanceOfPrecipi(Integer.parseInt(cursor.getString(5)));
+			} while (cursor.moveToNext());
+		}
+		return weatherList;
+
+	}
+
+	public void deleteWeather(Weather weather) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(TABLE_WEATHER, KEY_TIME + " = ?",
+				new String[] { String.valueOf(weather.getTime()) });
+		db.close();
 	}
 }
