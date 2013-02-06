be8f708f214fc6c3509e317f589203f54a06f838
Brian Lau <ir00ber@gmail.com>
2013-02-06 13:18:33 -0700

    Up-to-date test case of Weather object

--- src/com/example/globalresovlewws/WeatherTest.java
+++ src/com/example/globalresovlewws/WeatherTest.java
@@ -2,12 +2,18 @@ package com.example.globalresovlewws;
 
 import static org.junit.Assert.*;
 import org.junit.After;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WeatherTest {
-	private Weather weather1;
-	private Weather weather2;
+	private static Weather weather1;
+	private static Weather weather2;
 
+	@BeforeClass
+	public static void beforeClass(){
+		weather1 = new Weather();
+		weather2 = new Weather();
+	}
 	@Test
 	public void testConstructor() throws Exception {
 		weather1 = new Weather("12:32:01", 45.2, 78.1, 90, 78, 12);
@@ -29,8 +35,41 @@ public class WeatherTest {
 		assertEquals("02:56:32",weather1.getTime());
 		assertEquals("05:36:59",weather2.getTime());
 	}
-	@After
-	public void tearDown() throws Exception {
+	
+	@Test
+	public void testSetandGetLatitude(){
+		weather1.setLatitude(58.2);
+		weather2.setLatitude(74.1);
+		assertEquals(58.2,weather1.getLatitude(),.1);
+		assertEquals(74.1,weather2.getLatitude(),.1);
+	}
+	
+	@Test public void testSetandGetLongitude(){
+		weather1.setLongitude(87.6);
+		weather2.setLongitude(98.5);
+		assertEquals(87.6,weather1.getLongitude(),.1);
+		assertEquals(98.5,weather2.getLongitude(),.1);
+	}
+	
+	@Test public void testSetAndGetMaxTemp(){
+		weather1.setMaxTemp(100);
+		weather2.setMaxTemp(98);
+		assertEquals(100,weather1.getMaxTemp(),.1);
+		assertEquals(98,weather2.getMaxTemp(),.1);
+	}
+	
+	@Test public void testSetAndGetMinTemp(){
+		weather1.setMinTemp(20);
+		weather2.setMinTemp(-1);
+		assertEquals(20,weather1.getMinTemp(),.1);
+		assertEquals(-1,weather2.getMinTemp(),.1);
+	}
+	
+	@Test public void testSetAndGetCoP(){
+		weather1.setChanceOfPrecipi(50);
+		weather2.setChanceOfPrecipi(2);
+		assertEquals(50,weather1.getChanceOfPrecipi());
+		assertEquals(2,weather2.getChanceOfPrecipi());
 	}
 
 
